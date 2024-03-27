f6c3a8ef68b9324522f6a48a806df085f7da6fdf
Foez Ahmed <119846826+foez-ahmed@users.noreply.github.com>
2024-03-27 21:33:44 +0600

    Add files via upload
    Signed-off-by: Foez Ahmed <119846826+foez-ahmed@users.noreply.github.com>

---
+++ dependency_check.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "Runner Name : $1" >> temp
+echo "Host Name   : $(hostname)" >> temp
+echo "User Name   : $(whoami)" >> temp
+echo "Date        : $(date)" >> temp
+echo "" >> temp
+
+# COCOTB
+    which cocotb-config && \
+    echo -e "\033[1;32mcocotb\033[0m" >> temp || \
+    echo -e "\033[1;31mcocotb\033[0m" >> temp
+
+# GCC
+    which GCC && \
+    echo -e "\033[1;32mGNU Compiler Collections\033[0m" >> temp || \
+    echo -e "\033[1;31mGNU Compiler Collections\033[0m" >> temp
+
+# GNU Make
+    which make && \
+    echo -e "\033[1;32mGNU Make\033[0m" >> temp || \
+    echo -e "\033[1;31mGNU Make\033[0m" >> temp
+
+# Icarus Verilog
+    which iverilog && \
+    echo -e "\033[1;32mIcarus Verilog\033[0m" >> temp || \
+    echo -e "\033[1;31mIcarus Verilog\033[0m" >> temp
+
+# Python
+    echo "print('Hi')" > test.py
+    (python3 test.py && \
+    echo -e "\033[1;32mPython3\033[0m" >> temp) || \
+    (python test.py && \
+    echo -e "\033[1;32mPython\033[0m" >> temp) || \
+    echo -e "\033[1;31mPython\033[0m" >> temp
+    rm -rf test.py
+
+# Verilator
+    which vsim && \
+    echo -e "\033[1;32mIntel Questa\033[0m" >> temp || \
+    echo -e "\033[1;31mIntel Questa\033[0m" >> temp
+
+# Verilator
+    which verilator && \
+    echo -e "\033[1;32mVerilator\033[0m" >> temp || \
+    echo -e "\033[1;31mVerilator\033[0m" >> temp
+
+# Vivado
+    which vivado && \
+    echo -e "\033[1;32mXilinx Vivado\033[0m" >> temp || \
+    echo -e "\033[1;31mXilinx Vivado\033[0m" >> temp
+
+clear
+cat temp
+rm -rf temp
+
